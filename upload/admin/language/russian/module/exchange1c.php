9b9af4cac0fcf7e294ff986e4bdffcac4b1881b0
KirilLoveVE <vitaly.kirillove@mail.ru>
2015-10-25 23:11:58 +0800

    Create exchange1c.php

---
+++ upload/admin/language/russian/module/exchange1c.php
@@ -0,0 +1,78 @@
+<?php
+// Heading
+$_['heading_title']							= 'Обмен данными с 1C v8.x';
+
+// Text
+$_['text_module']							= 'Модули';
+$_['text_success']							= 'Настройки модуля обновлены!';
+
+$_['text_tab_general']						= 'Основное';
+$_['text_tab_product']						= 'Обмен товарами';
+$_['text_tab_order']						= 'Обмен заказами';
+$_['text_tab_manual']						= 'Ручная загрузка';
+
+$_['text_upload_success']					= 'Импорт завершен';
+$_['text_upload_error']						= 'Что-то пошло не так. Загляните в &laquo;Система &rarr; Журнал ошибок&raquo;';
+$_['text_max_filesize']						= 'Загружаемый файл не должен превышать %s Мб';
+
+$_['text_empty']							= 'Настроек пока нет';
+$_['text_homepage']							= 'Домашняя страничка модуля';
+$_['source_code']       					= 'Исходный код на GitHub';
+$_['text_price_default']					= 'Цена на сайте';
+
+
+// Вкладка "Основное"
+$_['entry_username']						= 'Логин:';
+$_['entry_password']						= 'Пароль:';
+$_['entry_status']							= 'Статус:';
+$_['entry_allow_ip']						= 'Разрешенные IP адреса (Разделять переносом строки. Если пусто, разрешены все адреса.):';
+
+// Таблицы
+$_['entry_config_price_type']				= 'Тип цены в 1С';
+$_['entry_customer_group']					= 'Группа покупателей';
+$_['entry_quantity']						= 'Количество';
+$_['entry_priority']						= 'Приоритет';
+$_['entry_action']							= 'Действия';
+// Справка
+$_['help_allow_ip']							= 'С каких IP разрешен доступ';
+
+$_['entry_flush_product']					= 'Сбрасывать товары:';
+$_['entry_flush_category']					= 'Сбрасывать категории:';
+$_['entry_flush_manufacturer']				= 'Сбрасывать производителей:';
+$_['entry_flush_quantity']					= 'Сбрасывать количество товаров:';
+$_['entry_flush_attribute']					= 'Сбрасывать атрибуты:';
+$_['entry_fill_parent_cats']				= 'Заполнять родительские категории:';
+$_['entry_seo_url']							= 'Генерировать SEO URL: <br> требуется модуль <a href="http://opencartforum.ru/files/file/332-deadcow-seo-v21-automod/" target="_blank">Deadcow SEO</a>)';
+$_['entry_seo_url_deadcow']					= 'через Deadcow SEO';
+$_['entry_seo_url_translit']				= 'Транслитерация';
+
+$_['entry_full_log']						= 'Включить подробный лог загрузки';
+$_['entry_apply_watermark']					= 'Накладывать водяные знаки при загрузке';
+$_['text_image_manager']					= 'Менеджер изображений';
+$_['text_browse']							= 'Обзор';
+$_['text_clear']							= 'Очистить';
+$_['text_source_code']						= 'Исходный код на GitHub';
+$_['text_change']							= 'Доработка и поддержка: Кириллов Виталий (Skype: KirilLoveVE, ICQ: 39927648, Viber: +79144306000, email: vitaly.kirillove@mail.ru)';
+
+$_['entry_order_status_to_exchange'] 		= 'Выгружать заказы со статусом:';
+$_['entry_order_status_to_exchange_not'] 	= '- не использовать -';
+$_['entry_relatedoptions']					= 'Загружать характеристики как связанные опции (требуется модуль <a href="http://opencartforum.ru/files/file/1501-связанные-опции/">Связанные опции</a>):';
+$_['entry_relatedoptions_help']				= 'в настройках связанных опций обязательно должна быть включены галки: "Пересчитывать количество", "Обновлять опции", "Использовать различные варианты связанных опций" ';
+$_['entry_dont_use_artsync'] 				= 'Не искать товары по артикулам:';
+
+$_['entry_order_status'] 					= 'Статус выгруженых заказов:';
+$_['entry_order_status_cancel'] 			= 'Статус отмененных заказов:';
+$_['entry_order_status_completed']			= 'Статус выполненных заказов:';
+$_['entry_order_notify']					= 'Уведомлять пользователей о смене статуса:';
+$_['entry_order_currency'] 					= 'Обозначение валюты (руб.):';
+$_['entry_upload']							= 'Выберите файл *.XML для загрузки';
+
+// Кнопки
+$_['button_upload']							= 'Загрузить';
+$_['button_add']							= 'Добавить';
+
+// Error
+$_['error_permission']						= 'У Вас нет прав для управления этим модулем!';
+
+// Справка
+$_['help_upload']							= 'Принимается import.xml, offers.xml, orders.xml. <br>Названия файлов может отличаться.';
