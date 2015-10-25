dbbb339a5738f0d6695e654f161fc8081ab19f0c
KirilLoveVE <vitaly.kirillove@mail.ru>
2015-10-25 23:11:21 +0800

    Create exchange1c.php

---
+++ upload/admin/language/english/exchange1c.php
@@ -0,0 +1,66 @@
+<?php
+// Heading
+$_['heading_title']	= 'Exchange 1C v8.x';
+
+// Text
+$_['text_module']		= 'Modules';
+$_['text_success']		= 'Settings saved!';
+
+$_['text_tab_general']		= 'General';
+$_['text_tab_product']		= 'Products';
+$_['text_tab_order']		= 'Orders';
+$_['text_tab_manual']		= 'Manual import';
+
+$_['text_upload_success']	= 'Import completed';
+$_['text_upload_error']		= 'Unknown error';
+$_['text_max_filesize']		= 'Max upload size %s MB';
+
+$_['text_empty']		= 'Settings are missing';
+$_['text_homepage']		= 'Module homepage';
+$_['source_code']       = 'Source code on GitHub';
+$_['text_price_default']	= 'Price for OpenCart';
+
+
+// Entry
+$_['entry_username']		= 'Login:';
+$_['entry_password']		= 'Password:';
+$_['entry_status']		= 'Status:';
+$_['entry_allow_ip']		= 'Allow IPs (Share a line break. If empty, all addresses are allowed.):';
+
+$_['entry_config_price_type']		= 'Price type:';
+$_['entry_customer_group']	= 'Customer group:';
+$_['entry_quantity']		= 'Quantity:';
+$_['entry_priority']		= 'Priority:';
+$_['entry_flush_product']	= 'Flush products:';
+$_['entry_flush_category']	= 'Flush categories:';
+$_['entry_flush_manufacturer']	= 'Flush manufacturers:';
+$_['entry_flush_quantity']	= 'Flush quantity:';
+$_['entry_flush_attribute']	= 'Flush attributes:';
+$_['entry_fill_parent_cats']	= 'Fill parent categories:';
+$_['entry_seo_url']		= 'Generate SEO URL: <br> (depends on <a href="http://opencartforum.ru/files/file/332-deadcow-seo-v21-automod/" target="_blank">Deadcow SEO</a>)';
+$_['entry_seo_url_deadcow']		= 'via Deadcow SEO';
+$_['entry_seo_url_translit']	= 'Transliterate';
+
+$_['entry_full_log']		= 'Enable log';
+$_['entry_apply_watermark']	= 'Apply watermark';
+$_['text_image_manager'] = 'Image Manager';
+$_['text_browse']        = 'Browse Files';
+$_['text_clear']         = 'Clear Image';
+
+$_['entry_order_status_to_exchange'] 	= 'Orders status to exchange:';
+$_['entry_order_status_to_exchange_not'] 	= "- don not use -";
+$_['entry_relatedoptions']	= 'Load characteristics as related options (need extension <a href="http://opencartforum.ru/files/file/1501-связанные-опции/">Related Options</a>):';
+$_['entry_relatedoptions_help']	= 'Related options settings should be turned on: "Recalc quantity", "Update options", "Use different related options variants" ';
+$_['entry_dont_use_artsync'] = 'Do not search products by sku:';
+
+$_['entry_order_status']	= 'Uploaded orders status:';
+$_['entry_order_status_cancel']	= 'Status of canceled orders:';
+$_['entry_order_status_completed']	= 'Status of completed orders:';
+$_['entry_order_notify']	= 'Notify users of status change:';
+$_['entry_order_currency']	= 'Orders currency:';
+
+$_['entry_upload']		= 'Select file:';
+$_['button_upload']		= 'Upload';
+
+// Error
+$_['error_permission']		= 'Premission denied!';
